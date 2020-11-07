24857900232f9f9ca062e670fedb8e860e0ec28b
xiangyanjun <qq5236088>
2020-11-07 17:15:20 +0800

    first

---
+++ boke-service/src/main/java/com/easy/boke/bokeservice/BokeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.easy.boke.bokeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BokeServiceApplication.class, args);
+    }
+
+}
---
+++ boke-service/src/test/java/com/easy/boke/bokeservice/BokeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.boke.bokeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
